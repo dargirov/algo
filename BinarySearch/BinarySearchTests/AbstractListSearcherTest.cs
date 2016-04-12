32e55b736bb6dc3013833972327f0c2b09255029
d.argirov <argirov@outlook.com>
2016-04-12 21:24:02 +0300

    added binary seacrh abstract test

---
+++ BinarySearch/BinarySearchTests/AbstractListSearcherTest.cs
@@ -0,0 +1,31 @@
+﻿namespace BinarySearchTests
+{
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using IListSearcher;
+
+    [TestClass]
+    public abstract class AbstractListSearcherTest
+    {
+        private static string[] values = { "B", "C", "D", "F", "H", "I", "J", "K", "L", "M", "P", "Q" };
+        private List<string> list;
+        private IListSearcher<string> searcher;
+
+        protected abstract IListSearcher<string> CreateSearcher();
+
+        protected void Setup()
+        {
+            this.searcher = this.CreateSearcher();
+            this.list = new List<string>(values);
+        }
+
+        [TestMethod]
+        public void SearchForExistingValuesTest()
+        {
+            for (int i = 0; i < this.list.Count; i++)
+            {
+                Assert.AreEqual(i, this.searcher.Search(this.list, this.list[i]));
+            }
+        }
+    }
+}
---
+++ BinarySearch/IListSearcher/IListSearcher.cs
@@ -0,0 +1,9 @@
+﻿namespace IListSearcher
+{
+    using System.Collections.Generic;
+
+    public interface IListSearcher<T>
+    {
+        int Search(IList<T> list, T key);
+    }
+}
